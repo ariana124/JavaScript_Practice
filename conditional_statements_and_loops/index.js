9a81f56a92c4265bd439f1125541c42b81f6e261
ariana124 <ariana.bibiano@gmail.com>
2020-07-23 15:47:49 -0700

    checks if number is negative

---
+++ conditional_statements_and_loops/index.js
@@ -0,0 +1,21 @@
+// The function checkSign checks if the number has a negative sign in front of it.
+
+function checkSign(num1, num2, num3) {
+    // Checks to make sure all of the inputs are numbers.
+    if ( typeof(num1) !== "number" || typeof(num2) !== "number" || typeof(num3) !== "number" ) {
+        return alert("One of the inputs is not a number");
+    }
+
+    product = num1 * num2 * num3;
+
+    if (product < 0) {
+        return alert("The sign is -.");
+    } else {
+        return alert("The number is positive.")
+    }
+}
+
+console.log(checkSign(3, 3, 5));
+console.log(checkSign(-6, 1, 2 ));
+console.log(checkSign('a', -8, 0 ));
+console.log(checkSign(true, 9, -7 ));
