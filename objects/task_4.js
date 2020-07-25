da3e067c6c6d15727ca6c5ef624840bc3184e1a6
ariana124 <ariana.bibiano@gmail.com>
2020-07-25 16:00:21 -0700

    checks reading status property of an object

---
+++ objects/task_4.js
@@ -0,0 +1,35 @@
+// The function readingStatus checks if the readingStatus property of an object is true or not and displays the corresponding message. O(n)
+
+function readingStatus(objArray) {
+
+    for (let i = 0; i < objArray.length; i++) {
+        let temp = objArray[i];
+        let readingStatus = temp.readingStatus;
+
+        if (readingStatus === true) {
+            console.log(`Already read ${temp.title} by ${temp.author}.`);
+        } else {
+            console.log(`You still need to read ${temp.title} by ${temp.author}.`)
+        }
+    }
+}
+
+let library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }
+];
+
+readingStatus(library);
