a4f83e573bc51cb7ae81ca0019e0636cd65ceca4
ariana124 <ariana.bibiano@gmail.com>
2020-07-24 17:54:02 -0700

    removed extra spaces and fixed comments

--- conditional_statements_and_loops/task_4.js
+++ conditional_statements_and_loops/task_4.js
@@ -1,14 +1,14 @@
-// The function findLargest finds the largest value in a group of numbers.
+// The function findLargest finds the largest value in a group of five numbers.
 
 function findLargest(num1, num2, num3, num4, num5) {
     // Check to make sure all inputs are numbers.
-    if ( typeof(num1) !== "number" || typeof(num2) !== "number" || typeof(num3) !== "number" || typeof(num4) !== "number" || typeof(num5) !== "number" ) {
+    if (typeof(num1) !== "number" || typeof(num2) !== "number" || typeof(num3) !== "number" || typeof(num4) !== "number" || typeof(num5) !== "number") {
         return alert("One of the inputs is not a number");
     }
 
     let numbers = [num1, num2, num3, num4, num5];
 
-    // Return the largest value in the array.
-    // The spread operator (...) breaks up the array into its elements.
+    // Returns an alert box with the largest value in the array.
+    // The spread operator (...) breaks up the array into individual elements.
     return alert(Math.max(...numbers));
 }
