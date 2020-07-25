8feef381b06adcd18e1a6a2613b86b5ed056e3b7
ariana124 <ariana.bibiano@gmail.com>
2020-07-24 17:28:04 -0700

    added new edge case for number check

--- string_exercises/task_4.js
+++ string_exercises/task_4.js
@@ -1,14 +1,17 @@
 // The function truncateString returns a truncated string.
 
 function truncateString(string, number) {
-    if (typeof(string) !== "string") {
+    if (typeof(string) !== "string" || typeof(number) !== "number") {
         return -1;
     }
 
-    // It slices starting from the 1st index to the number index - 1. 
+    // It slices the string starting from the 1st index to the number index - 1. 
     let newString = string.slice(0, number);
 
     return newString;
 }
 
 console.log(truncateString("White Night", 3));
+console.log(truncateString("Fly Away With Me", "5"));
+console.log(truncateString("WANNABE", -3));
+console.log(truncateString(true, 3));
