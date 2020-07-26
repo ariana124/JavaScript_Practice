dfcffc895b8e52d8e5a1faced597f55c9b10c749
ariana124 <ariana.bibiano@gmail.com>
2020-07-26 15:18:36 -0700

    quicksort algorithm

---
+++ sorting_algorithms/quicksort.js
@@ -0,0 +1,39 @@
+// The quicksort function sorts an array using the quicksort algorithm.
+
+function quicksort(array) {
+    // If the array is empty or only has one element there is no need to sort.
+    if (array.length < 2) {
+        return array;
+    } else {
+        let pivot = array[0];
+        let less = [];
+        let greater = [];
+        array = array.slice(1);
+
+        for (let i = 0; i < array.length; i++) {
+            // If the current value at the current index is less than the pivot we add that value to the less array.
+            if (array[i] <= pivot) {
+                less.push(array[i]);
+            }
+            // If the current value at the current index is greater than the pivot we add that value to the greater array.
+            if (array[i] > pivot) {
+                greater.push(array[i]);
+            }
+        }
+
+        less = quicksort(less);
+        greater = quicksort(greater);
+
+        let sortedArray = less.concat(pivot, greater);
+        //let sortedArray = [...less, pivot, ...greater]; This also works.
+
+        return sortedArray;
+    }
+}
+
+
+console.log(quicksort([17, 8, 1, 3, 5]));
+console.log(quicksort([-7, 8, 1, 3, 5]));
+console.log(quicksort([]));
+console.log(quicksort([9]));
+console.log(quicksort([-7, 8, 1, -3, 5]));
