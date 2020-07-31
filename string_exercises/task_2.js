e6914cd2f5b2ceed75c634cb1838a954b585f0f8
ariana124 <ariana.bibiano@gmail.com>
2020-07-30 20:25:29 -0700

    edited code description

--- string_exercises/task_2.js
+++ string_exercises/task_2.js
@@ -1,5 +1,8 @@
-// The function isBlack checks if a string is empty or not.
-
+/**
+ * isBlank - function that checks if a string is empty or not
+ * @string: a string
+ * Return: true if the string is empty, false if it is not, and -1 if the input is not a string
+ */
 function isBlank(string) {
     if (typeof(string) !== "string") {
         return -1;
@@ -16,3 +19,4 @@ console.log(isBlank("string"));
 console.log(isBlank("s"));
 console.log(isBlank("4"));
 console.log(isBlank(""));
+console.log(isBlank(9));
--- string_exercises/task_4.js
+++ string_exercises/task_4.js
@@ -1,7 +1,15 @@
-// The function truncateString returns a truncated string.
-
+/**
+ * truncateString - function that truncates a string
+ * @string: a string
+ * @number: number of characters in truncated string
+ * Return: the truncated string or -1 for anything else
+ */
 function truncateString(string, number) {
     if (typeof(string) !== "string" || typeof(number) !== "number") {
+        return -1;
+    }
+
+    if (number < 0) {
         return -1;
     }
 
@@ -13,5 +21,6 @@ function truncateString(string, number) {
 
 console.log(truncateString("White Night", 3));
 console.log(truncateString("Fly Away With Me", "5"));
-console.log(truncateString("WANNABE", -3));
+console.log(truncateString("WANNABE ME", -3));
 console.log(truncateString(true, 3));
+console.log(truncateString("Bittersweet", 6));
