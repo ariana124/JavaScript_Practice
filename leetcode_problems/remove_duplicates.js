dd5d4408e9edcd2cf6142139c67d2c0ea2e862e0
ariana124 <ariana.bibiano@gmail.com>
2020-07-30 19:53:56 -0700

    changed parameter name and added code description

--- leetcode_problems/remove_duplicates.js
+++ leetcode_problems/remove_duplicates.js
@@ -1,11 +1,13 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * removeDuplicates - function that removes duplicates in an array
+ * @array: an array
+ * Return: the length of the array without duplicates
  */
-let removeDuplicates = function(nums) {
+let removeDuplicates = function(array) {
     // Set removes duplicates in an array.
-    return new Set(nums).size;
+    return new Set(array).size;
 };
 
 
 console.log(removeDuplicates([1,2,2,3,4]));
+console.log(removeDuplicates(['apple', 'apple', 'peach', 'cherry']));
