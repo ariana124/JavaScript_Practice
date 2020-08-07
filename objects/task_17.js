a523f7df66c254e8773fdbde192cf83a1d1279d5
ariana124 <ariana.bibiano@gmail.com>
2020-08-06 20:46:29 -0700

    checks if an object has a specific property

---
+++ objects/task_17.js
@@ -0,0 +1,21 @@
+/**
+ * checkProperty - function that checks if an object has a specific property
+ * @obj: an object containing key, value pairs
+ * @property: the property of the object to check for
+ * Return: true if the property exists in the object or false if it doesn't exist 
+ */
+function checkProperty(obj, property) {
+
+    // let check = obj.hasOwnProperty(property);
+    // return check;
+
+    if (property in obj) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(checkProperty({animal: 'dog', drink: 'water'}, 'drink'));
+console.log(checkProperty({animal: 'dog', drink: 'water'}, 'food'));
