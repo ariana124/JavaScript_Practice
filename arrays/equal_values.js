b27ec572d55edafb716be46156c510143f540a02
ariana124 <ariana.bibiano@gmail.com>
2020-08-23 20:44:13 -0700

    created another implementation using the every method

--- arrays/equal_values.js
+++ arrays/equal_values.js
@@ -4,10 +4,19 @@
  * Return: true if all the values are equal to each other, otherwise false
  */
 function equalArrayValues(array) {
-    // The every method tets whether all elements in the array pass the test by the provided function.
-    const equal = array.every(element => element === array[0]);
-    
-    return equal;
+    // The every method tests whether all elements in the array pass the test determined by the provided function.
+    // The shorthand version of the below implementation.
+    // const equal = array.every(element => element === array[0]);
+
+    const equalOrNot = array.every(function(element) {
+        if (array[0] === element) {
+            return true;
+        } else {
+            return false;
+        }
+    })
+
+    return equalOrNot;
 }
 
 console.log(equalArrayValues([1, 1, 1]));
